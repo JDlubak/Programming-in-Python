f93aa3d588615ea6220b01855244449ec98d9ec3
JDlubak <kuba.dl@wp.pl>
2025-11-10 19:25:51 +0100

    Old code clean up

--- Task 2/ui.py
+++ Task 2/ui.py
@@ -1,16 +1,3 @@
-
-
-def print_all():
-    print1()
-    print2()
-    print3()
-
-
-def print_sheep_was_eaten(sheep_id: int) -> None:
-    print(f'Eaten sheep: {sheep_id}')
-    return
-
-
 def print_round_result(r: int,
                        wolf_x_pos: float,
                        wolf_y_pos: float,
@@ -21,15 +8,3 @@ def print_round_result(r: int,
     print(f'Wolf position: {wolf_x_pos:.3f}, {wolf_y_pos:.3f}')
     print(f'Alive sheep: {sheep_counter}')
     print(f'{action} sheep id: {wolf_action["sheep_id"]}')
-
-
-def print1():
-    return None
-
-
-def print2():
-    return None
-
-
-def print3():
-    return None
